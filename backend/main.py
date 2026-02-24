cd522a75098d8786c0d205887fb74e5e2f6341e9
Tim <timon.skvor@mail,ru>
2026-02-24 21:45:26 +0300

    refactor: update configs for backend and frontend

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # адрес фронтенда
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
