12672737de566211540c2cb2d2728e66837c84d8
Tim <timon.skvor@mail,ru>
2026-04-21 19:43:25 +0300

    refactor: fix style in log panel

--- frontend/src/components/LogPanel.tsx
+++ frontend/src/components/LogPanel.tsx
@@ -190,42 +190,58 @@ export default function LogPanel<T>({title, logs, columns, filters, onDownload, 
                 {/* logs / table */}
                 <div
                     ref={scrollRef}
-                    className="flex-1 overflow-y-auto px-6 py-4 space-y-2 font-mono text-sm text-gray-600"
+                    className="flex-1 overflow-y-auto px-6 space-y-2 font-mono text-sm text-gray-600"
                 >
 
                     {columns ? (
-                        <table className="w-full table-auto border-collapse text-left">
-                            <thead className="sticky top-0 z-10 bg-white">
-                            <tr>
-                                {columns.map(col => (
-                                    <th
-                                        key={String(col.key)}
-                                        className="border-b px-2 py-2 font-medium text-gray-700 bg-white"
-                                    >
-                                        {col.label}
-                                    </th>
-                                ))}
-                            </tr>
-                            </thead>
+                        <div className="relative">
+                            <table className="w-full table-auto border-collapse text-left">
 
-                            <tbody>
-                            {safeLogs.map((log, i) => (
-                                <tr key={i} className="hover:bg-gray-50">
+                                {/* sticky header + линия вместе */}
+                                <thead className="sticky top-0 z-20 bg-white">
+                                <tr>
                                     {columns.map(col => (
-                                        <td
+                                        <th
                                             key={String(col.key)}
-                                            className="px-2 py-1 border-l-2 bg-white"
-                                            style={{ borderColor: RED }}
+                                            className="px-2 pt-5 pb-1 font-medium text-gray-700 bg-white"
                                         >
-                                            {col.render
-                                                ? col.render(log[col.key], log)
-                                                : String(log[col.key])}
-                                        </td>
+                                            {col.label}
+                                        </th>
                                     ))}
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
+
+                                <tr>
+                                    <th colSpan={columns.length} className="p-0">
+                                        <div
+                                            className="h-[1.5px] w-full"
+                                            style={{
+                                                backgroundColor: RED
+                                            }}
+                                        />
+                                    </th>
+                                </tr>
+                                </thead>
+
+                                <tbody>
+                                {safeLogs.map((log, i) => (
+                                    <tr key={i} className="hover:bg-gray-50">
+                                        {columns.map(col => (
+                                            <td
+                                                key={String(col.key)}
+                                                className="px-2 py-1 border-l-2 bg-white"
+                                                style={{ borderColor: RED }}
+                                            >
+                                                {col.render
+                                                    ? col.render(log[col.key], log)
+                                                    : String(log[col.key])}
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))}
+                                </tbody>
+
+                            </table>
+                        </div>
                     ) : (
                         safeLogs.map((log, i) => (
                             <div key={i} className="pl-3 border-l-2" style={{borderColor: RED}}>
